1aef0fe604b19583b0a37852ea02ae74689a7b61
keshavk27 <keshavk.2027@gmail.com>
2025-01-13 21:55:54 +0530

    initialised the project

---
+++ quizzers/src/components/github/github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function Github(){
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/keshavk27')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            setData(data)
+
+        })
+    },[])
+
+    return (
+        <>
+        <div className="text-center m-4 bg-gray-500 text-white p-3 text-3xl">GitHub Id: {data.name} </div>
+        <div className="text-center m-4 bg-gray-500 text-white p-3 text-3xl">GitHub follower: {data.followers} </div>
+        </>
+    )
+}
+
+export default Github
---
+++ quizzers/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider,createRoutesFromElements, Route } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Home from './components/home/home.jsx'
+import About from './components/AboutUs/About.jsx'
+import ContactUs from './components/contact/ContactUs.jsx'
+import User from './components/user/user.jsx'
+import Github from './components/github/github.jsx'
+
+const router=createBrowserRouter(  
+  createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='contact' element={<ContactUs/>}/>
+    <Route path='user/:id' element={<User/>}/>
+    <Route path='github' element={<Github/>}/>
+  </Route>
+)
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
