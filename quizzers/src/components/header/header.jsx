19ddfa24db55d109735627093d85b31f3bcc392d
keshavk27 <keshavk.2027@gmail.com>
2025-01-15 10:07:07 +0530

    header updated

--- quizzers/src/components/header/header.jsx
+++ quizzers/src/components/header/header.jsx
@@ -7,20 +7,20 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
+                            src="https://eu-central.storage.cloudconvert.com/tasks/5ddd5a9d-53c9-45c5-ab6b-c2965429d63d/Screenshot%202025-01-15%20003302.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20250114%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20250114T190321Z&X-Amz-Expires=86400&X-Amz-Signature=0edeb8698662a1ec2db3de37ee538e3195f357d95272da431b0967c0d5ca6b77&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%22Screenshot%202025-01-15%20003302.png%22&response-content-type=image%2Fpng&x-id=GetObject"
+                            className="mr-2 h-12"
                             alt="Logo"
                         />
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="login"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
                         <Link
-                            to="#"
+                            to="signup"
                             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
@@ -48,7 +48,27 @@ export default function Header() {
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-500" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    About
+                                    About Us
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/createquiz"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-500" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Create Quiz
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/allquiz"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-500" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    All Quiz
                                 </NavLink>
                             </li>
                             <li>
@@ -61,6 +81,7 @@ export default function Header() {
                                     ContactUs
                                 </NavLink>
                             </li>
+
                             <li>
                                 <NavLink
                                     to="/github"
